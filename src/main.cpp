41b5cef611c5b53afe4ae40031a3113b592b8008
Jan200101 <sentrycraft123@gmail.com>
2019-05-05 03:00:51 +0200

    add testing source, modify main to be testable

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <QApplication>
 #include "interface.hpp"
 
+#ifdef UNIT_TEST
+#include "catch2/catch.hpp"
+#endif
+
+#ifndef UNIT_TEST
 int main(int argc, char** argv)
 {
     QApplication App(argc, argv);
@@ -9,3 +14,4 @@ int main(int argc, char** argv)
 
     return App.exec();
 }
+#endif
---
+++ src/test.cpp
@@ -0,0 +1,4 @@
+#ifdef UNIT_TEST
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch2/catch.hpp"
+#endif
