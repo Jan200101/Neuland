cedd4802bdbd3358daf44e255aeaecfbb72d25b5
Jan200101 <sentrycraft123@gmail.com>
2019-05-09 07:38:33 +0200

    Remove remainds of unit testing suite

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include <QApplication>
 #include "interface.hpp"
 
-#ifdef UNIT_TEST
-#include "catch2/catch.hpp"
-#endif
-
-#ifndef UNIT_TEST
 int main(int argc, char** argv)
 {
     QApplication App(argc, argv);
@@ -14,4 +9,3 @@ int main(int argc, char** argv)
 
     return App.exec();
 }
-#endif
