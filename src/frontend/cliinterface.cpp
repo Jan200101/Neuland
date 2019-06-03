Jan200101/Neuland

commit f1e84531d056e820567803fe8044ffb27023038f

implement file information into CLI interface
from now on almost all development time will go into the CLI interface just to spite someone else
there might be some development to it done after 2019-06-19 but don't count on it

src/frontend/cliinterface.cpp
@@ -1,5 +1,5 @@
 #include <ncurses/ncurses.h>
-#include <iostream>
+#include <fstream>
 
 #include "defines.hpp"
 
@@ -7,6 +7,7 @@
 
 #include "backend/config.hpp"
 #include "backend/dirs.hpp"
+#include "backend/files.hpp"
 
 #define TEXTCOLOR 1
 #define TITLECOLOR 2
@@ -21,7 +22,8 @@
  *
  * @return     WINDOW*
  */
-WINDOW* createWin(int height, int width, int starty, int startx)
+WINDOW*
+createWin(int height, int width, int starty, int startx)
 {
     WINDOW* win;
 
@@ -98,6 +100,10 @@ CliWindow::CliWindow(int argc, char** argv)
  */
 int CliWindow::exec()
 {
+    std::ifstream file;
+    Json::Value card;
+    std::string categories;
+
     WINDOW* win = nullptr;
     WINDOW* list = nullptr;
 
@@ -201,17 +207,38 @@ int CliWindow::exec()
 
         short textpos = 0;
 
+        mvprintw(4 + textpos, 7, "Title");
+        mvprintw(4 + textpos, 20, "Anzahl");
+        mvprintw(4 + textpos, 30, "Kategorien");
+
         for (auto& p : Backend::listCarddir())
         {
+            categories.clear();
+            file.open(p.path().c_str());
+            card = Backend::parseFile(file);
+
             if ((3 + textpos++) > LINES - 8)
                 break;
             if (cursorpos + 1 == textpos)
             {
                 attron(A_BOLD);
-                mvprintw(3 + textpos, 5, "*");
+                mvprintw(4 + textpos, 5, "*");
                 attroff(A_BOLD);
             }
-            mvprintw(3 + textpos, 7, "%s", p.path().stem().c_str());
+
+            int size = card.get("categories", Json::Value()).size();
+            for (int i = 0; i < size; ++i)
+            {
+                if (!categories.empty())
+                    categories += " ,";
+                categories += card.get("categories", Json::Value())[i].asString();
+            }
+
+            mvprintw(4 + textpos, 7, "%s", p.path().stem().c_str());
+            mvprintw(4 + textpos, 20, "%u", card.get("cards", Json::Value()).size());
+            mvprintw(4 + textpos, 30, "%s", categories.c_str());
+
+            file.close();
         }
 
         attron(A_UNDERLINE);

src/main.cpp
@@ -62,9 +62,9 @@ int main(int argc, char** argv)
     int (*app)(int&, char**) = DEFAULTINTERFACE;
 
     // create environment needed
-    Backend::makeConfigdir();
-    Backend::makeCarddir();
-    Backend::makeConfigfile();
+    Backend::makeConfigdir();  // create main config directory
+    Backend::makeCarddir();    // create card dir in the config directory
+    Backend::makeConfigfile(); // create config file
 
     // update lastrun value in config
     {

commit e59460fb5977754e720b4779c2633a38e01fc6c9

realign function
this issue has been introduced in f1e84531d056e820567803fe8044ffb27023038f by bad clang-format settings

src/frontend/cliinterface.cpp
@@ -22,8 +22,7 @@
  *
  * @return     WINDOW*
  */
-WINDOW*
-createWin(int height, int width, int starty, int startx)
+WINDOW* createWin(int height, int width, int starty, int startx)
 {
     WINDOW* win;
 